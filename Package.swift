d8b6f4228886907403e030b610d8e8554849b2ad
Christian Treffs <ctreffs@gmail.com>
2021-01-13 15:26:45 +0100

    Add swift-crypto

--- Package.swift
+++ Package.swift
@@ -11,14 +11,22 @@ swiftSettings = nil
 
 let package = Package(
     name: "FirebladeUUID",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
+    ],
     products: [
         .library(
             name: "FirebladeUUID",
             targets: ["FirebladeUUID"])
     ],
+    dependencies: [.package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.3")],
     targets: [
         .target(
             name: "FirebladeUUID",
+            dependencies: [.product(name: "Crypto", package: "swift-crypto")],
             swiftSettings: swiftSettings),
         .testTarget(
             name: "FirebladeUUIDTests",
